11135317c6820fb5f03c6c16f0d36778017d77b0
yg <56058845+yunki-park@users.noreply.github.com>
2024-12-09 11:30:29 +0900

    storybook 초기 설정
    npx sb init

---
+++ pwa-react/plopfile.js
@@ -0,0 +1,26 @@
+module.exports = function generateComponent(plop) {
+  plop.setGenerator("component", {
+    description: "Create a component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "컴포넌트 이름을 입력해주세요.",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/index.tsx",
+        templateFile: "plop-templates/component.hbs",
+        // overwrite: true,
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
+        templateFile: "plop-templates/story.hbs",
+        // overwrite: true,
+      },
+    ],
+  });
+};
