432fd9dda025c1a62aa212d039265910c17ff290
yg <56058845+yunki-park@users.noreply.github.com>
2024-12-09 17:53:45 +0900

    SignInCard breakpoint 관련 부분 함수화

--- pwa-react/src/components/sign-in-side/SignInCard.tsx
+++ pwa-react/src/components/sign-in-side/SignInCard.tsx
@@ -14,6 +14,12 @@ import { styled } from "@mui/material/styles";
 import ForgotPassword from "./ForgotPassword";
 import { GoogleIcon, FacebookIcon } from "./CustomIcons";
 
+const cardBreakpointStyles = (theme: any) => ({
+  [theme.breakpoints.up("sm")]: {
+    width: "450px",
+  },
+});
+
 const Card = styled(MuiCard)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
@@ -23,9 +29,7 @@ const Card = styled(MuiCard)(({ theme }) => ({
   gap: theme.spacing(2),
   boxShadow:
     "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
-  [theme.breakpoints.up("sm")]: {
-    width: "450px",
-  },
+  ...cardBreakpointStyles(theme),
   ...theme.applyStyles("dark", {
     boxShadow:
       "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
